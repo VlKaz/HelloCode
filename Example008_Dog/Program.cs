0e594c17cd5d73c0f2a04ede13dcc7e62eb85984
VlKaz <1768433@gmail.com>
2023-07-31 11:38:44 +0300

    add example 008 dog

---
+++ Example008_Dog/Program.cs
@@ -0,0 +1,28 @@
+﻿int count = 0;
+Console.Write("Введите растояние между друзьями в метрах:");
+string dist = Console.ReadLine();
+int distance = int.Parse(dist);
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend =2;
+int time;
+
+
+while(distance>10)
+{
+    if(friend ==1)
+    {
+        time = distance/(firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance/(secondFriendSpeed+dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed)*time;
+    count++;
+}
+Console.Write("Количество раз которое собака сбегала туда обратно: ");
+Console.Write(count);
